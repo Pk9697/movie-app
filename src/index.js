e6856fd4553ab6edd5875bc0fa6d697cd37cdd21
Prashant Kumar <prashant.aman9@gmail.com>
2021-08-18 13:39:31 +0530

    use currying for adding middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore,applyMiddleware } from 'redux';
 //we keep our file imports below package imports
 import './index.css';
 import App from './components/App';
 import combineReducers from './reducers';//optional combineReducers will be added default in create store cos we added default property to combineReducers 
+//using currying here of fxn logger(obj,next,action)
+//logger(obj)(next)(action) each part returns a fxn in which we pass next set of arguments one by one
+const logger=function({dispatch,getState}){//this fxn receives an argument obj which contains dispatch and getState properties//same as that of store which redux will pass in this logger fxn
+  return function(next){//next is used to switch to next middleware when multiple middlewares are present
+    return function(action){
+      //middleware code
+      console.log('ACTION_TYPE=',action.type);
+      next(action);//calling next middleware with action as arguments if next middleware is present otherwise next will call dispatch with action as argument
+    }
+  }
+}
 
-const store=createStore(combineReducers);//store requires reducer to be passed to it
+const store=createStore(combineReducers,applyMiddleware(logger));//store requires reducer to be passed to it
 console.log("store:",store);
 // console.log("BEFORE STATE",store.getState());//getting state from reducers 
 // store.dispatch({
